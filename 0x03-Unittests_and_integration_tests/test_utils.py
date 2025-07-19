6bf9bd2407faaf07bec668b4a6c375a5798e2ede
Bemnet57 <bemnetasseged57@gmail.com>
2025-07-19 21:51:41 +0300

    updated utils.py with assertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map
-"""
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """TestCase for the access_nested_map function"""
+    """Test cases for the access_nested_map function"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -15,5 +13,38 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Tests access_nested_map returns expected value"""
+        """Test that access_nested_map returns correct values"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that access_nested_map raises KeyError for invalid paths"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+
+
+
+
+# #!/usr/bin/env python3
+# """Unit tests for utils.access_nested_map
+# """
+# import unittest
+# from parameterized import parameterized
+# from utils import access_nested_map
+
+
+# class TestAccessNestedMap(unittest.TestCase):
+#     """TestCase for the access_nested_map function"""
+
+#     @parameterized.expand([
+#         ({"a": 1}, ("a",), 1),
+#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
+#         ({"a": {"b": 2}}, ("a", "b"), 2),
+#     ])
+#     def test_access_nested_map(self, nested_map, path, expected):
+#         """Tests access_nested_map returns expected value"""
+#         self.assertEqual(access_nested_map(nested_map, path), expected)
