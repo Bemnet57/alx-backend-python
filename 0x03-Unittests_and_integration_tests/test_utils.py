b22371836c89696532519e1e695d9b1517942652
Bemnet57 <bemnetasseged57@gmail.com>
2025-07-19 22:27:19 +0300

    fixing checker errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,7 +7,6 @@ from utils import get_json
 from utils import memoize
 
 
-
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for the access_nested_map function"""
 
@@ -33,12 +32,14 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
     """Test cases for the get_json function"""
 
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, test_payload):
-        """Test that get_json returns expected payload without real HTTP call"""
+        """Test that get_json returns expected payload 
+            without real HTTP call"""
         with patch("utils.requests.get") as mock_get:
             mock_response = Mock()
             mock_response.json.return_value = test_payload
@@ -74,23 +75,3 @@ class TestMemoize(unittest.TestCase):
             mock_method.assert_called_once()
 
 
-
-# #!/usr/bin/env python3
-# """Unit tests for utils.access_nested_map
-# """
-# import unittest
-# from parameterized import parameterized
-# from utils import access_nested_map
-
-
-# class TestAccessNestedMap(unittest.TestCase):
-#     """TestCase for the access_nested_map function"""
-
-#     @parameterized.expand([
-#         ({"a": 1}, ("a",), 1),
-#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-#         ({"a": {"b": 2}}, ("a", "b"), 2),
-#     ])
-#     def test_access_nested_map(self, nested_map, path, expected):
-#         """Tests access_nested_map returns expected value"""
-#         self.assertEqual(access_nested_map(nested_map, path), expected)
