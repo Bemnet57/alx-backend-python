c00de7d6f8150b3b501217888aa26796560959cf
Bemnet57 <bemnetasseged57@gmail.com>
2025-07-19 22:47:34 +0300

    error checks

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Unit tests for utils.py functions: access_nested_map, get_json, and memoize.
+"""
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
