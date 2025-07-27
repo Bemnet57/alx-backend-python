4031f1977b6657d74a05fdd71f499b1a40f894fd
Bemnet57 <bemnetasseged57@gmail.com>
2025-07-27 16:44:02 +0300

    error checks

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -22,8 +22,21 @@ Including another URLconf
 # ]
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from .views import ConversationViewSet, MessageViewSet
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('messaging_app.chats.urls')),  # <-- use full import path
+# ]
+# from django.urls import path, include
+# from rest_framework import routers
+# from .views import ConversationViewSet, MessageViewSet
+
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('messaging_app.chats.urls')),  # <-- use full import path
+    path('', include(router.urls)),
 ]
