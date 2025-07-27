b439683bc535a86c720a492029361113db9ecaad
Bemnet57 <bemnetasseged57@gmail.com>
2025-07-27 16:16:30 +0300

    error checks

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -14,21 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-#added
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ConversationViewSet, MessageViewSet
-
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
-
-urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include('messaging_app.chats.urls')),  # <-- use full import path
 ]
