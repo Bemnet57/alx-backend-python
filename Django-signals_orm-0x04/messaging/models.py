993ca7ce6b81a964d6c976ffa22345539fe060c2
Bemnet57 <bemnetasseged57@gmail.com>
2025-08-01 18:27:04 +0300

    added notification model

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -70,3 +70,12 @@ class Message(models.Model):
     def __str__(self):
         return f"{self.sender} → {self.receiver}: {self.content[:20]}"
 
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username} - Message ID {self.message.id}"
