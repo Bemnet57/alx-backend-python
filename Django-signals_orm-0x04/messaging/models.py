666e27835ebf0c4495b82687eaffac2b9e41e0a1
Bemnet57 <bemnetasseged57@gmail.com>
2025-08-01 19:02:53 +0300

    added MessageHistory model

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -80,3 +80,13 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user.username} - Message ID {self.message.id}"
+    
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='edit_history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Edit of Message ID {self.message.id} at {self.edited_at}"
+
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -2,6 +2,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message, Notification
+from django.db.models.signals import pre_save
+from .models import Message, MessageHistory
+from django.dispatch import receiver
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
