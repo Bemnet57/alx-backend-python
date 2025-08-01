ed1ba5ac3f2b448c4d89a19af27bf3a2f50fc803
Bemnet57 <bemnetasseged57@gmail.com>
2025-08-01 19:06:53 +0300

    added a pre_save signal to log edits

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -13,3 +13,14 @@ def create_notification_on_message(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:
+        old_instance = Message.objects.get(pk=instance.pk)
+        if instance.content != old_instance.content:
+            MessageHistory.objects.create(
+                message=instance,
+                old_content=old_instance.content
+            )
+            instance.edited = True
